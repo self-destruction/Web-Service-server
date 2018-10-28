1cb7e8d123f29221e6675ee532e7dd507411c42e
Максим Сырчиков <niibadzov@gmail.com>
2018-10-28 22:24:44 +0500

    added object in parameter

--- scripts.sh
+++
@@ -1,9 +0,0 @@
-go run greeter_server/main.go
-node greeter_client/greeter_client.js
-protoc -I helloworld/ helloworld/helloworld.proto --go_out=plugins=grpc:helloworld
-
-
-go run main.go
-node greeter_client.js
-
-protoc -I proto/ helloworld.proto --go_out=plugins=grpc:proto
